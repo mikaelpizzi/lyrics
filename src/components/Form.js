b2593b1481756d032ce2df3891859d04fa4b7b7f
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-16 23:36:21 -0400

    [FEAT] Add form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,50 @@
 const Form = () => {
     return (  
-        <h1>Form</h1>
-    );
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    >
+                        <fieldset>
+                            <legend className="text-center">Song Lyrics Search Engine</legend>
+
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Artist</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="artist"
+                                            placeholder="Artist Name"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Song</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="song"
+                                            placeholder="Song Name"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            >Search</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+        );
 }
  
 export default Form;
