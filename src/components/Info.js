a46ef81b283672d9fad6fd8f6218f2ed39c90802
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-17 12:37:35 -0400

    [FEAT] Show artist social media

--- src/components/Info.js
+++ src/components/Info.js
@@ -13,6 +13,17 @@ const Info = ({ info }) => {
                 <p className="card-text">Genre: {strGenre}</p>
                 <h2 className="card-text">Biography:</h2>
                 <p className="card-text">{strBiographyEN}</p>
+                <p className="card-text">
+                    <a href={`https://${info.strFacebook}`} target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-facebook"></i>
+                    </a>
+                    <a href={`https://${info.strTwitter}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href={`${info.strLastFMChart}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-lastfm"></i>
+                    </a>
+                </p>
             </div>
         </div>
     );
